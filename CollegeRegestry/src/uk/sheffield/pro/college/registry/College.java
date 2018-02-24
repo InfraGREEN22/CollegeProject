a25097633ba00cabdf6ec324ceec81f3a2cbafbc
Alex <36669271+InfraGREEN22@users.noreply.github.com>
2018-02-24 17:36:40 +0000

    Update
    Added GUI and features for adding a student and searching for students according to defined criteria

--- CollegeRegestry/src/uk/sheffield/pro/college/registry/College.java
+++ CollegeRegestry/src/uk/sheffield/pro/college/registry/College.java
@@ -4,16 +4,14 @@ import java.util.ArrayList;
 
 public class College {
     private String collegeName;             // college name; the college should have a name, shouldn't it?
-    private ArrayList<Student> students;    // a "container" for college students
+    public ArrayList<Student> students;    // a "container" for college students
 
-    public College(String name) {
-        this.collegeName = name;
+    public College() {
         this.students = new ArrayList<>();
     }
 
     // add a student to the college
-    public void addStudent(String name, String surname) {
-        Student std = new Student(name,surname);
+    public void addStudent(Student std) {
         this.students.add(std);
     }
 }
--- CollegeRegestry/src/uk/sheffield/pro/college/registry/Student.java
+++ CollegeRegestry/src/uk/sheffield/pro/college/registry/Student.java
@@ -7,12 +7,21 @@ public class Student {
     private String familyName;  // student surname
     private int id;             // student id
     private String programme;   // either FIY or PMP
+    private String course;      // either SE or BSSH
+    private String pathway;     // student's pathway
     private double gpa;         // student's GPA
+    private String nationality; // student's nationality
+    private String gender;      // student's gender
 
     // constructor
-    public Student(String name, String surname) {
+    public Student(String name, String surname, String gender, String nationality, String programme, String course, String pathway) {
         this.firstName = name;
         this.familyName = surname;
+        this.gender = gender;
+        this.nationality = nationality;
+        this.programme = programme;
+        this.course = course;
+        this.pathway = pathway;
         Random id = new Random();
         this.setId(id.nextInt((999999-100000)+1)+100000);  //setting a random number from 100000 to 999999 to make it more unique
     }
@@ -51,6 +60,26 @@ public class Student {
         return gpa;
     }
 
+    // get student's gender
+    public String getGender() {
+        return gender;
+    }
+
+    // get student's nationality
+    public String getNationality() {
+        return nationality;
+    }
+
+    // get student's course
+    public String getCourse() {
+        return course;
+    }
+
+    // get student's pathway
+    public String getPathway() {
+        return pathway;
+    }
+
     /////////////////////////////////////////////////
     //                   SETTERS                   //
     /////////////////////////////////////////////////
@@ -84,5 +113,25 @@ public class Student {
     // set student's programme (either FIY or PMP)
     public void setProgramme(String programme) {
         this.programme = programme;
+    }
+
+    // set student's gender
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    // set student's nationality
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    // set student's pathway
+    public void setPathway(String pathway) {
+        this.pathway = pathway;
+    }
+
+    // set student's course
+    public void setCourse(String course) {
+        this.course = course;
     }
 }
